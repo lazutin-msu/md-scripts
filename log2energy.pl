ece9e9900ff0f8beef5a484c0d5494840ce6bba2
Alexei Lazutin <lazutin@polly.phys.msu.ru>
2022-12-21 17:13:51 +0300

    log2energy.pl makes clear thermo output from log.lammps

---
+++ log2energy.pl
@@ -0,0 +1,74 @@
+#!/usr/bin/perl
+use Getopt::Long;
+
+GetOptions (
+'f|file=s' => \$in_file, 
+'o|out=s' => \$outfile, 
+'h|help' => \$help
+);
+
+if($help) {
+  print <<END;
+Options for this program:
+-f --file for input file default= dump.all
+-o --output for output file 
+END
+  exit;
+  }
+
+open IN, "<$in_file" or die "Cannot open $in_file: $!";
+open OUT, ">$outfile" or die "Cannot open $outfile: $!" ;
+
+while($str=<IN>)
+ {
+ if($str =~ /run 1000000/) 
+   {
+   #print "run found\n"; 
+   last;
+   }
+ }
+
+while($str=<IN>)
+ {
+ if($str =~ /Step/) 
+   {
+   print OUT $str; 
+   #print "title found\n"; 
+   last;}
+ }
+
+while($str=<IN>)
+ {
+ if($str =~ /Loop/) 
+   {
+   last;
+   }
+   else
+   {
+   print OUT $str;
+   }
+ }
+
+while(!eof(IN))
+{
+while($str=<IN>)
+ {
+ if($str =~ /Step/) { last;}
+ }
+
+while($str=<IN>)
+ {
+ if($str =~ /Loop/) 
+   {
+   last;
+   }
+   else
+   {
+   print OUT $str;
+   }
+ }
+}
+
+
+close(IN);
+close(OUT);
